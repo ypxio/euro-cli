58cc88c82d43ea08e633e5e4a19d6b260e1b8561
Yuri Pratama <yuripertamax@gmail.com>
2016-07-04 00:05:15 +0700

    Delete all non-api-call function

--- api.js
+++ api.js
@@ -1,38 +1,45 @@
 var RequestPromise = require('request-promise');
 var API_URL = "http://api.football-data.org";
 
-function FootballData(apiToken) {
-  if(!(this instanceof FootballData))
-    return new FootballData(apiToken);
-    this.apiToken = apiToken;
+
+class FootballData {
+  
+  constructor(apiToken) {
+    this.apiToken = apiToken
+  }
+  
+  getRequest(path) {
+    return {
+      uri: API_URL + path,
+      headers: {
+          'X-Auth-Token': this.apiToken
+      },
+      json: true
+    }
+  }
+
+  getSession(sessionID) {
+    return RequestPromise(this.getRequest(`/v1/soccerseasons/${sessionID}`))
+  }
+
+  getCountryLists(sessionID) {
+    return RequestPromise(this.getRequest(`/v1/soccerseasons/${sessionID}/teams`))
+  }
+
+  getCountryFixtures(countryID) {
+    return RequestPromise(this.getRequest(`/v1/teams/${countryID}/fixtures`));
+  }
+
+  getCountryID(countryURL) {
+    var countryURLSplitted = countryURL.split("/");
+    var countryID = countryURLSplitted[countryURLSplitted.length-1];
+    return countryID;
+  }
+
+  getGroupDetails(sessionID) {
+    return RequestPromise(this.getRequest(`/v1/soccerseasons/${sessionID}/leagueTable`));
+  }
+
 }
 
-FootballData.prototype.getRequest = function (path) {
-  return {
-    uri: API_URL + path,
-    headers: {
-        'X-Auth-Token': this.apiToken
-    },
-    json: true
-  };
-}
-
-FootballData.prototype.getSession = function (sessionID) {
-  return RequestPromise(this.getRequest(`/v1/soccerseasons/${sessionID}`));
-}
-
-FootballData.prototype.getCountryLists = function (sessionID) {
-  return RequestPromise(this.getRequest(`/v1/soccerseasons/${sessionID}/teams`));
-}
-FootballData.prototype.getCountryID = function (countryURL) {
-  var countryURLSplitted = countryURL.split("/");
-  var countryID = countryURLSplitted[countryURLSplitted.length-1];
-  return countryID;
-}
-FootballData.prototype.getCountryFixtures = function (countryID) {
-  return RequestPromise(this.getRequest(`/v1/teams/${countryID}/fixtures`));
-}
-FootballData.prototype.getGroupDetails = function (sessionID) {
-  return RequestPromise(this.getRequest(`/v1/soccerseasons/${sessionID}/leagueTable`));
-}
 module.exports = FootballData;
